0c5ad9000664878bf66290eedfad5c0452ce57da
Andy Connolly <andrew.c.connolly@dartmouth.edu>
2021-02-08 12:08:23 -0500

    Edit: main readme

--- Capture/C++/VideoCapture.cpp
+++ Capture/C++/VideoCapture.cpp
@@ -1,36 +1,40 @@
 ﻿/************************************************************************************************/
-// VideoCapture.cpp : Detects if device connected or changes state, if video input available,
-// then it calls ffmpeg to start recording video. If it changes state while recording it stops 
-// recording and begins a new recording if possible.
+// VideoCapture.cpp : Detects if device connected or changes state, if video
+// input available, then it calls ffmpeg to start recording video. If it changes
+// state while recording it stops recording and begins a new recording if
+// possible.
 //
-// This code is adapted and modified from the Magewell example code cited below. All licensing
-// and legal matters pertaining thereunto are stipulated in the text below which has been preserved 
-// from the original Magewell distribution.
+// This code is adapted and modified from the Magewell example code cited below.
+// All licensing and legal matters pertaining thereunto are stipulated in the
+// text below which has been preserved from the original Magewell distribution.
 //
-// Code bastardizer: Andy Connolly : andrew.c.connolly@dartmouth.edu
-// date of bastardization inception: 01/15/2020
+// Code bastardizer: Andy Connolly : andrew.c.connolly@dartmouth.edu date of
+// bastardization inception: 01/15/2020
 //
-// Original header begins after this line.
-// USBDeviceDetect.cpp : Defines the entry point for the console application.
-// MAGEWELL PROPRIETARY INFORMATION
-// The following license only applies to head files and library within Magewell’s SDK 
-// and not to Magewell’s SDK as a whole. 
+// <Original header begins after this line.>  
 //
-// Copyrights © Nanjing Magewell Electronics Co., Ltd. (“Magewell”) All rights reserved.
+// USBDeviceDetect.cpp : Defines the
+// entry point for the console application.  MAGEWELL PROPRIETARY INFORMATION
+// The following license only applies to head files and library within
+// Magewell’s SDK and not to Magewell’s SDK as a whole. 
 //
-// Magewell grands to any person who obtains the copy of Magewell’s head files and library 
-// the rights,including without limitation, to use, modify, publish, sublicense, distribute
-// the Software on the conditions that all the following terms are met:
-// - The above copyright notice shall be retained in any circumstances.
-// -The following disclaimer shall be included in the software and documentation and/or 
-// other materials provided for the purpose of publish, distribution or sublicense.
+// Copyrights © Nanjing Magewell Electronics Co., Ltd. (“Magewell”) All rights
+// reserved.
 //
-// THE SOFTWARE IS PROVIDED BY MAGEWELL “AS IS” AND ANY EXPRESS, INCLUDING BUT NOT LIMITED TO,
-// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-// IN NO EVENT SHALL MAGEWELL BE LIABLE 
+// Magewell grands to any person who obtains the copy of Magewell’s head files
+// and library the rights,including without limitation, to use, modify, publish,
+// sublicense, distribute the Software on the conditions that all the following
+// terms are met: - The above copyright notice shall be retained in any
+// circumstances.  -The following disclaimer shall be included in the software
+// and documentation and/or other materials provided for the purpose of publish,
+// distribution or sublicense.
 //
-// FOR ANY CLAIM, DIRECT OR INDIRECT DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT,
-// TORT OR OTHERWISE, ARISING IN ANY WAY OF USING THE SOFTWARE.
+// THE SOFTWARE IS PROVIDED BY MAGEWELL “AS IS” AND ANY EXPRESS, INCLUDING BUT
+// NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL MAGEWELL BE LIABLE 
+//
+// FOR ANY CLAIM, DIRECT OR INDIRECT DAMAGES OR OTHER LIABILITY, WHETHER IN
+// CONTRACT, TORT OR OTHERWISE, ARISING IN ANY WAY OF USING THE SOFTWARE.
 //
 // CONTACT INFORMATION:
 // SDK@magewell.net
