cee3af1a1313615cb10762d12941021717bf6e89
Horea Christian <chr@chymera.eu>
2023-07-26 13:19:20 -0400

    Not attempting roundtrip testing in 1-board mode

--- Events/conveyor.py
+++ Events/conveyor.py
@@ -3,7 +3,14 @@ from mpremote import pyboard
 from time import time
 
 pyb = pyboard.Pyboard("/dev/ttyACM0", 115200)
-pyb_debug = pyboard.Pyboard("/dev/ttyACM1", 115200)
+try:
+	pyb_debug = pyboard.Pyboard("/dev/ttyACM1", 115200)
+except pyboard.PyboardError:
+	print("Only one board is connected, live roundtrip delay testing will be disabled by default.")
+	CHECK_DELAY = False
+else:
+	CHECK_DELAY = True
+
 
 def listen(command):
 	"""
@@ -34,7 +41,7 @@ def no_listen(command):
 	pyb_debug.enter_raw_repl()
 	result = pyb_debug.exec_raw_no_follow(command)
 
-def timed_events(check_delay=True):
+def timed_events(check_delay=CHECK_DELAY):
 	dts = []
 	dtbase = None
 	ntrials = 0
