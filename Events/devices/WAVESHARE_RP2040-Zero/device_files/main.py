ba0a32ffda3c9978ddb30d3c9eb7e72334c340a6
Norman Sackett <normansackett@gmail.com>
2023-07-26 19:28:36 -0400

    changed debug pin to 7 for waveshare

--- Events/devices/WAVESHARE_RP2040-Zero/device_files/main.py
+++ Events/devices/WAVESHARE_RP2040-Zero/device_files/main.py
@@ -1,10 +1,10 @@
 from machine import Pin
 import time
-p29 = Pin(29, Pin.OUT)
-p29.value(0)
+power = Pin(7, Pin.OUT)
+power.value(0)
 
 
 def send_debug_signal():
-    p29.value(1)
+    power.value(1)
     time.sleep(1)
-    p29.value(0)
+    power.value(0)
