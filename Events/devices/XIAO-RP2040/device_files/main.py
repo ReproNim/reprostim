2a59632130022d33b8794c2f4731607d8e32bd06
Horea Christian <chr@chymera.eu>
2023-08-14 14:34:28 -0400

    Better variable names

--- Events/devices/XIAO-RP2040/device_files/main.py
+++ Events/devices/XIAO-RP2040/device_files/main.py
@@ -1,15 +1,16 @@
 from machine import Pin
 import time
-power = Pin(1, Pin.OUT)
-power.value(0)
+debug_pin = Pin(1, Pin.OUT)
+debug_pin.value(0)
 
+# Pls check whether our roundtrip delay is larger if the Pin is defined in the function as opposed to global scope.
 def send_debug_signal():
-	power.value(1)
-	time.sleep(1)
-	power.value(0)
+    debug_pin.value(1)
+    time.sleep(1)
+    debug_pin.value(0)
 
 def send_predefined_event(mypin=0, myduration=1):
-	power = Pin(mypin, Pin.OUT)
-	power.value(1)
-	time.sleep(myduration)
-	power.value(0)
+    event_pin = Pin(mypin, Pin.OUT)
+    event_pin.value(1)
+    time.sleep(myduration)
+    event_pin.value(0)
