3cc32190d3630cc805692df9b2cabe6325a2ee49
Horea Christian <chr@chymera.eu>
2023-08-17 11:58:25 -0400

    Shorter sleep time

--- Events/devices/XIAO-RP2040/device_files/main.py
+++ Events/devices/XIAO-RP2040/device_files/main.py
@@ -6,7 +6,7 @@ debug_pin.value(0)
 # Pls check whether our roundtrip delay is larger if the Pin is defined in the function as opposed to global scope.
 def send_debug_signal():
     debug_pin.value(1)
-    time.sleep(1)
+    time.sleep(0.001)
     debug_pin.value(0)
 
 def send_predefined_event(mypin=0, myduration=1):
