642d2202136a6b90bed1cd4514ffacf095336d73
ReproStim virtual user <reprostim@databeast.centerforopenneuroscience.org>
2023-09-22 14:33:58 -0400

    [DATALAD RUNCMD] Make a copy of the roundtripper since it needed per device ATM
    === Do not change lines below ===
    {
     "chain": [],
     "cmd": "cp devices/WAVESHARE_RP2040-Zero/device_files/roundtripper.py devices/XIAO-RP2040/device_files/",
     "exit": 0,
     "extra_inputs": [],
     "inputs": [],
     "outputs": [],
     "pwd": "Events"
    }
    ^^^ Do not change lines above ^^^

---
+++ Events/devices/XIAO-RP2040/device_files/roundtripper.py
@@ -0,0 +1,32 @@
+from machine import Pin
+import time
+debug_pin = Pin(29, Pin.OUT)
+debug_pin.value(0)
+
+# Pls check whether our roundtrip delay is larger if the Pin is defined in the function as opposed to global scope.
+def send_debug_signal():
+	debug_pin.value(1)
+	time.sleep(1)
+	debug_pin.value(0)
+
+def send_predefined_event(mypin=0, myduration=1):
+	event_pin = Pin(mypin, Pin.OUT)
+	event_pin.value(1)
+	time.sleep(myduration)
+	event_pin.value(0)
+
+def selfreport():
+	pin_send = Pin(0, Pin.OUT)
+	pin_receive = Pin(1, Pin.IN)
+	is_roundtripper = True
+	pin_send.value(0)
+	if pin_receive.value() != 0:
+		is_roundtripper = False
+	pin_send.value(1)
+	if pin_receive.value() != 1:
+		is_roundtripper = False
+	pin_send.value(0)
+	if pin_receive.value() != 0:
+		is_roundtripper = False
+	print(is_roundtripper)
+	print(repr("KILLLISTENER"))
