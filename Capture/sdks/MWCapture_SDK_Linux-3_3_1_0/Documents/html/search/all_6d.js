d739b4aac77aca5e99cea9d0ca82b62e098b9f9c
Vadim Melnik <vmelnik@docsultant.com>
2023-12-14 16:39:59 +0200

    Migrate to newer Magewell SDK #63, (moved sdks to 3rdparty directory).

--- Capture/sdks/MWCapture_SDK_Linux-3_3_1_0/Documents/html/search/all_6d.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['mw_5fdma_5fdesc',['mw_dma_desc',['../structmw__dma__desc.html',1,'']]],
-  ['mw_5fdma_5fmemory_5fclient',['mw_dma_memory_client',['../structmw__dma__memory__client.html',1,'']]]
-];
--- Capture/sdks/MWCapture_SDK_Linux-3_3_1_0/Include/common/Product.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "ProductVer.h"
-
-#define PRODUCT_NAME		"Magewell USB Capture Utility\0"
-#define LEGAL_TRADEMARKS	"Magewell (TM)\0"
