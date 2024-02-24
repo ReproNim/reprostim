88e69ef22f67cedacc4a51b623ef3ca4c0d532fd
Vadim Melnik <vmelnik@docsultant.com>
2024-02-24 15:33:00 +0200

    Tests for *CaptureApp constructor/destructor, WiP #74.

--- Capture/screencapture/test/TestScreenCapture.cpp
+++ Capture/screencapture/test/TestScreenCapture.cpp
@@ -12,10 +12,20 @@ int add(int a, int b) {
 	return a + b;
 }
 
-TEST_CASE("TestScreenCapture_add", "[add]") {
+TEST_CASE("TestScreenCapture_add",
+		  "[screencapture][add]") {
 	// Test case 1
 	REQUIRE(add(2, 2) == 4);
 
 	// Test case 2
 	REQUIRE(add(-1, 1) == 0);
+}
+
+TEST_CASE("TestScreenCapture_constructor_destructor",
+		  "[screencapture][ScreenCaptureApp][constructor][destructor") {
+	std::unique_ptr<ScreenCaptureApp> pApp(new ScreenCaptureApp());
+	REQUIRE(pApp != nullptr);
+
+	pApp = nullptr;
+	REQUIRE(pApp == nullptr);
 }
--- Capture/videocapture/test/TestVideoCapture.cpp
+++ Capture/videocapture/test/TestVideoCapture.cpp
@@ -12,10 +12,20 @@ int add(int a, int b) {
 	return a + b;
 }
 
-TEST_CASE("TestVideoCapture_add", "[add]") {
+TEST_CASE("TestVideoCapture_add",
+		  "[videocapture][add]") {
 	// Test case 1
 	REQUIRE(add(5, 3) == 8);
 
 	// Test case 2
 	REQUIRE(add(-10, 20) == 10);
+}
+
+TEST_CASE("TestVideoCapture_constructor_destructor",
+		  "[videocapture][VideoCaptureApp][constructor][destructor") {
+	std::unique_ptr<VideoCaptureApp> pApp(new VideoCaptureApp());
+	REQUIRE(pApp != nullptr);
+
+	pApp = nullptr;
+	REQUIRE(pApp == nullptr);
 }
