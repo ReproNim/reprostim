8dfac22affd3c90b3b5a60b86a3f91d79fcf3e7d
Yaroslav Halchenko <debian@onerussian.com>
2024-08-22 16:03:49 -0400

    Initial attempt to produce psychopy container -- some confligs between systemwide and conda libs for libgl etc

---
+++ containers/reprostim-timesync/generate_container.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -eu
+
+generate() {
+	# more details might come on https://github.com/ReproNim/neurodocker/issues/330
+	[ "$1" == singularity ] && add_entry=' "$@"' || add_entry=''
+	#neurodocker generate "$1" \
+	ndversion=1.0.1
+	#ndversion=master
+		#--ndfreeze date=20240801 \
+    # Thought to use conda-forge for this, but feedstock is not maintained:
+    #  https://github.com/conda-forge/psychopy-feedstock/issues/64
+    # --miniconda version=py312_24.5.0-0 conda_install="conda-forge::psychopy conda-forge::qrcode" \
+    # Had to go with 3.11 due to https://stackoverflow.com/questions/77364550/attributeerror-module-pkgutil-has-no-attribute-impimporter-did-you-mean
+    # Need extra -dev libraries etc to install/build wxpython
+	docker run --rm repronim/neurodocker:$ndversion generate "$1" \
+		--base-image=neurodebian:bookworm \
+		--pkg-manager=apt \
+		--install build-essential pkg-config \
+          libgtk-3-dev libwxgtk3.2-dev libwxgtk-media3.2-dev libwxgtk-webview3.2-dev libcanberra-gtk3-module \
+          libusb-1.0-0-dev portaudio19-dev libasound2-dev \
+          vim wget strace time ncdu gnupg curl procps pigz less tree python3 python3-pip \
+        --miniconda version=py311_24.5.0-0 conda_install="conda-forge::qrcode" pip_install="psychopy psychtoolbox" \
+		--user=reproin \
+#        \
+#		--run "$run_cmd" \
+#		--run "apt-get update && apt-get -y dist-upgrade" \
+#		--run "curl -sL https://deb.nodesource.com/setup_20.x | bash - " \
+#		--install nodejs \
+#		--run "npm install -g bids-validator@1.14.5" \
+#		--run "mkdir /afs /inbox" \
+#		--copy bin/reproin /usr/local/bin/reproin \
+#		--run "chmod a+rx /usr/local/bin/reproin" \
+#		--entrypoint "/usr/local/bin/reproin$add_entry"
+}
+
+generate docker > Dockerfile
+generate singularity > Singularity
