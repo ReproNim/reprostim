44589ca04a7d5d79d5cb2d1ca6f043c1695efb5a
Vadim Melnik <vmelnik@docsultant.com>
2024-09-17 18:52:13 +0300

    CPU/resources usage statistics for ffmpeg with options we used before, #68

--- Capture/research/ffmpeg_enc/ffmpeg_enc.sh
+++ Capture/research/ffmpeg_enc/ffmpeg_enc.sh
@@ -50,7 +50,11 @@
 #/usr/bin/time -v ffmpeg -f alsa -t 600 -ac 2 -thread_queue_size 4096 -i hw:1,1 -f v4l2 -t 600 -framerate 60 -video_size 1920x1080 -i /dev/video0 -c:v libx264 -flush_packets 1 -preset ultrafast -crf 18 -r 60 -tune zerolatency -b:v 2M -maxrate 2M -bufsize 4M  -acodec aac -vf setpts=PTS-STARTPTS -af asetpts=PTS-STARTPTS output007.mp4
 
 # video capture with x264 optimizations: ultrafast, crf=18 zerolatency
-echo "Test 008"
-rm output008.mp4
-/usr/bin/time -v ffmpeg -f alsa -t 15 -ac 2 -thread_queue_size 4096 -i hw:1,1 -f v4l2 -t 15 -framerate 60 -video_size 1920x1080 -i /dev/video0 -c:v libx264 -flush_packets 1 -preset ultrafast -crf 18 -r 60 -tune zerolatency -b:v 2M -maxrate 2M -bufsize 4M  -acodec aac -vf setpts=PTS-STARTPTS -af asetpts=PTS-STARTPTS output008.mp4
+#echo "Test 008"
+#rm output008.mp4
+#/usr/bin/time -v ffmpeg -f alsa -t 15 -ac 2 -thread_queue_size 4096 -i hw:1,1 -f v4l2 -t 15 -framerate 60 -video_size 1920x1080 -i /dev/video0 -c:v libx264 -flush_packets 1 -preset ultrafast -crf 18 -r 60 -tune zerolatency -b:v 2M -maxrate 2M -bufsize 4M  -acodec aac -vf setpts=PTS-STARTPTS -af asetpts=PTS-STARTPTS output008.mp4
 
+# video capture similar to initial one we have before changes
+echo "Test 009"
+rm output009.mp4
+/usr/bin/time -v ffmpeg -f alsa -t 15 -ac 2 -thread_queue_size 4096 -i hw:1,1 -f v4l2 -t 15 -input_format yuyv422 -framerate 60 -video_size 1920x1080 -thread_queue_size 4096 -i /dev/video0 -c:v libx264 -flush_packets 1 output009.mkv
