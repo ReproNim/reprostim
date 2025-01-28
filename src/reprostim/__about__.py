413f8562f8306154026ef123c4d81db155991443
Vadim Melnik <vmelnik@docsultant.com>
2025-01-28 18:40:18 +0200

    Fixed hatch versioningit configuration to include autogenerated _version.py file in target dist/wheel, #130.

--- src/reprostim/__about__.py
+++ src/reprostim/__about__.py
@@ -2,8 +2,11 @@
 #
 # SPDX-License-Identifier: MIT
 
-# flake8: noqa: F401
-from ._version import __version__
+try:
+    # flake8: noqa: F401
+    from ._version import __version__
+except ImportError:
+    __version__ = "0.0.0"  # Default version
 
 # specify the name of reprostim tool
 __reprostim_name__ = "reprostim"
