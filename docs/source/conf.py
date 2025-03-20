5f85fabd336c1aad0abbab089d31e55e166476a4
Vadim Melnik <vmelnik@docsultant.com>
2025-03-20 20:37:06 +0200

    Fixing RTD pyaudio dependency with mock, #125.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,14 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 # import os
-# import sys
+import sys
+
+# bypass pyaudio with mock
+from unittest.mock import MagicMock
+
+MOCK_MODULES = ["pyaudio"]
+sys.modules.update((mod_name, MagicMock()) for mod_name in MOCK_MODULES)
+
 # sys.path.insert(0, os.path.abspath("../../src"))
 
 
