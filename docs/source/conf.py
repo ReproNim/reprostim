510d5c43c5a287b11f2f70a4b7c90de63389f32a
Vadim Melnik <vmelnik@docsultant.com>
2025-03-20 12:06:36 +0200

    Switch to alabaster UI theme like used in HeuDiConv, #125.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,8 +46,8 @@ default_role = "py:obj"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "furo"
-# html_theme = 'alabaster'
+# html_theme = "furo"
+html_theme = "alabaster"
 # html_static_path = ['_static']
 
 # configure myst_parser
