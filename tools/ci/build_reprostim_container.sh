1cd531a15aa8640f1821859b8a81d834d9b66d62
Vadim Melnik <vmelnik@docsultant.com>
2025-05-16 15:56:21 +0300

    CI/CD container workflow action draft, WiP, #146.

--- tools/ci/build_reprostim_container.sh
+++ tools/ci/build_reprostim_container.sh
@@ -9,8 +9,8 @@ echo "Building containers for ReproStim.."
 cd ../../containers/repronim-reprostim
 pwd
 
-@echo execute generate_container.sh
+echo execute generate_container.sh
 ./generate_container.sh
 
-@echo execute build_singularity.sh
+echo execute build_singularity.sh
 ./build_singularity.sh
