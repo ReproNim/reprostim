3e868e98e4bfb3d8a2ab34a84d54d1f7cdb41f6b
Vadim Melnik <vmelnik@docsultant.com>
2025-07-14 13:35:36 +0300

    Make `reprostim-capture` CMake build working with both Catch2 v2 and v3, #147.

--- src/reprostim-capture/capturelib/test/TestCaptureLib.cpp
+++ src/reprostim-capture/capturelib/test/TestCaptureLib.cpp
@@ -1,7 +1,15 @@
 #define CATCH_CONFIG_MAIN
 #include <regex>
 #include "reprostim/CaptureLib.h"
-#include <catch2/catch_all.hpp>
+
+// Catch2 v2/v3 includes
+#if __has_include(<catch2/catch_all.hpp>)
+    // Catch2 v3
+    #include <catch2/catch_all.hpp>
+#else
+  // Catch2 v2 fallback
+  #include <catch2/catch.hpp>
+#endif
 
 using namespace reprostim;
 // Function to test
--- src/reprostim-capture/capturelib/test/TestCaptureThreading.cpp
+++ src/reprostim-capture/capturelib/test/TestCaptureThreading.cpp
@@ -1,6 +1,14 @@
 #include "reprostim/CaptureLib.h"
 #include "reprostim/CaptureThreading.h"
-#include <catch2/catch_all.hpp>
+
+// Catch2 v2/v3 includes
+#if __has_include(<catch2/catch_all.hpp>)
+    // Catch2 v3
+    #include <catch2/catch_all.hpp>
+#else
+  // Catch2 v2 fallback
+  #include <catch2/catch.hpp>
+#endif
 
 using namespace reprostim;
 
