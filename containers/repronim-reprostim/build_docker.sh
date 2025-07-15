111849d07fe212767aa838b0ec8f1c5c4d549d53
Vadim Melnik <vmelnik@docsultant.com>
2025-07-15 12:59:33 +0300

    Build docker image for `repronim-reprostim` container, #159, #163.

--- containers/repronim-reprostim/build_docker.sh
+++ containers/repronim-reprostim/build_docker.sh
@@ -2,4 +2,14 @@
 
 set -eu
 
-echo "TODO: build docker for ReproStim"
+REPROSTIM_VERSION=$(git describe --tags --abbrev=0 2>/dev/null || echo "0.0.0")
+REPROSTIM_SUFFIX_NOTAG=repronim-reprostim
+REPROSTIM_SUFFIX=${REPROSTIM_SUFFIX_NOTAG}-${REPROSTIM_VERSION}
+
+
+echo "Building docker container '${REPROSTIM_SUFFIX}' for ReproStim v${REPROSTIM_VERSION}.."
+#
+docker build -f Dockerfile.${REPROSTIM_SUFFIX_NOTAG} \
+  -t repronim/reprostim:${REPROSTIM_VERSION} \
+  -t repronim/reprostim:latest \
+  .
