8118149563258b4be45cdf640586d6119a3c299c
Vadim Melnik <vmelnik@docsultant.com>
2025-07-30 12:17:52 +0300

    Made docker tag with the latest and release number (based on git describe) for releases, and just master for all builds in master (not in PRs) #159, #163.

--- containers/repronim-reprostim/build_docker.sh
+++ containers/repronim-reprostim/build_docker.sh
@@ -6,6 +6,8 @@ set -eu
 REPROSTIM_VERSION=$(git describe --tags --abbrev=0 2>/dev/null || echo "0.0.0")
 REPROSTIM_SUFFIX_NOTAG=repronim-reprostim
 REPROSTIM_SUFFIX=${REPROSTIM_SUFFIX_NOTAG}-${REPROSTIM_VERSION}
+TAGS=${REPROSTIM_DOCKER_TAGS:-unstable}
+
 
 (
   set -e
@@ -14,11 +16,19 @@ REPROSTIM_SUFFIX=${REPROSTIM_SUFFIX_NOTAG}-${REPROSTIM_VERSION}
   cd ../../..
   echo "$PWD"
 
+  # Construct multiple -t options
+  TAG_ARGS=""
+  for TAG in $TAGS; do
+    TAG_ARGS+=" -t repronim/reprostim:$TAG"
+  done
+
   echo "Building docker container '${REPROSTIM_SUFFIX}' for ReproStim v${REPROSTIM_VERSION}.."
+  echo "Using tags: ${TAGS}"
+  echo "Using tag args: ${TAG_ARGS}"
+
   #
   docker build -f ./reprostim/containers/repronim-reprostim/Dockerfile.${REPROSTIM_SUFFIX_NOTAG} \
-    -t repronim/reprostim:${REPROSTIM_VERSION} \
-    -t repronim/reprostim:latest \
+    $TAG_ARGS \
     .
 )
 echo "Done."
