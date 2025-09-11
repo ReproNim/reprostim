774e4a0d3a3efc39e3154c860e42224c7d0aa6ab
Vadim Melnik <vmelnik@docsultant.com>
2025-09-11 18:03:20 +0300

    Updated `build_singularity.sh` script to work well both under user and root accounts, #175, #174.

--- containers/repronim-reprostim/build_singularity.sh
+++ containers/repronim-reprostim/build_singularity.sh
@@ -6,7 +6,13 @@ REPROSTIM_VERSION=$(git describe --tags --abbrev=0 2>/dev/null || echo "0.0.0")
 REPROSTIM_SUFFIX_NOTAG=repronim-reprostim
 REPROSTIM_SUFFIX=${REPROSTIM_SUFFIX_NOTAG}-${REPROSTIM_VERSION}
 
+# Use --fakeroot if not running as root
+if [ "$EUID" -eq 0 ]; then
+    FAKEROOT_ARG=""
+else
+    FAKEROOT_ARG="--fakeroot"
+fi
 
 echo "Building singularity container '${REPROSTIM_SUFFIX}.sing' for ReproStim v${REPROSTIM_VERSION}.."
 #
-singularity build --fakeroot ${REPROSTIM_SUFFIX}.sing Singularity.${REPROSTIM_SUFFIX_NOTAG}
+singularity build $FAKEROOT_ARG "${REPROSTIM_SUFFIX}.sing" "Singularity.${REPROSTIM_SUFFIX_NOTAG}"
