86cc305e1ed6571857b1aeb772cd241fef154df8
Vadim Melnik <vmelnik@docsultant.com>
2025-09-12 17:23:28 +0300

    Added sample session start QrCode and fixation cross, #160, #174.

--- tools/psychopy/01_fmri_interval.py
+++ tools/psychopy/01_fmri_interval.py
@@ -1,41 +1,68 @@
+from psychopy import core, event, logging, visual
 from psychopy_mri_emulator import launchScan
-from psychopy import visual, core, event, logging
+
+from reprostim.qr.psychopy import EventType, QrCode, QrConfig, QrStim
 
 logging.console.setLevel(logging.INFO)
 
 # Create a window
-win = visual.Window([800, 600], fullscr=False)
+win = visual.Window([800, 600], units="pix", fullscr=False)
+
+# Optionally specify a QR code config
+qr_config = QrConfig(
+    scale=0.3,
+    duration=2.1,
+    padding=30,
+    align="right-bottom",
+)
+
+# Create and draw sample fixation cross
+fix = visual.TextStim(win, text="+", pos=(0, 0))
+fix.draw()
+win.flip()
 
 # Create a global clock to sync
 globalClock = core.Clock()
 
 # MRI scanner settings
 MR_settings = {
-    'TR': 0.2,       # time between volumes (sec)
-    'volumes': 5,    # number of volumes to simulate
-    'sync': '5',     # key representing scanner pulse
-    'skip': 0,
-    'sound': False
+    "TR": 0.2,  # time between volumes (sec)
+    "volumes": 5,  # number of volumes to simulate
+    "sync": "5",  # key representing scanner pulse
+    "skip": 0,
+    "sound": False,
 }
 
 logging.info("Starting fMRI scan emulator in test interval mode")
+# Display a QR code indicating the start of the session
+# and wait for its duration
+qr = QrStim(win, QrCode(EventType.SESSION_START), qr_config)
+fix.draw()
+qr.draw()
+win.flip()
+qr.wait()
+
+# Restore fixation cross
+fix.draw()
+win.flip()
+
+core.wait(2)
 
 seq = 0
 # Wait for first sync pulse (Test mode will generate them automatically)
-seq += launchScan(win, MR_settings, globalClock=globalClock,
-           mode='Test',  log=True)
+seq += launchScan(win, MR_settings, globalClock=globalClock, mode="Test", log=True)
 logging.info(f"Pulse[0], initial detected at {globalClock.getTime():.3f} sec")
 
 # Main loop to react to pulses
 while True:
     keys = event.getKeys()
-    if MR_settings['sync'] in keys:
+    if MR_settings["sync"] in keys:
         logging.info(f"Pulse[{seq}] detected at {globalClock.getTime():.3f} sec")
         seq += 1
-        if seq >= MR_settings['volumes']:
+        if seq >= MR_settings["volumes"]:
             logging.info("All volumes acquired.")
             break
-    if 'escape' in keys:
+    if "escape" in keys:
         logging.info("Experiment terminated by user (Escape pressed)")
         break
 
