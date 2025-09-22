808547c93c266111eb60c984859646cdad3aa42f
Vadim Melnik <vmelnik@docsultant.com>
2025-09-23 00:00:43 +0300

    Try to hold Linux runtime packages necessary for `reprostim-videocapture` ,#184, #185.

--- containers/repronim-reprostim/generate_container.sh
+++ containers/repronim-reprostim/generate_container.sh
@@ -27,7 +27,11 @@ if [[ "$REPROSTIM_CAPTURE_ENABLED" == "1" ]]; then
   REPROSTIM_CAPTURE_PACKAGES_DEV="libyaml-cpp-dev libspdlog-dev catch2 libv4l-dev libudev-dev libopencv-dev libcurl4-openssl-dev nlohmann-json3-dev cmake g++"
   REPROSTIM_CAPTURE_PACKAGES_RUNTIME="libyaml-cpp0.7 libfmt9"
   REPROSTIM_CAPTURE_BUILD="cd \"$REPROSTIM_HOME/src/reprostim-capture\"; mkdir build; cd build; cmake ..; make; cd ..; cmake --install build; rm -rf \"$REPROSTIM_HOME/src/reprostim-capture/build\"; reprostim-videocapture -V"
-  REPROSTIM_CAPTURE_CLEAN="apt-get remove --purge -y $REPROSTIM_CAPTURE_PACKAGES_DEV && apt-get autoremove -y && apt-get install -y $REPROSTIM_CAPTURE_PACKAGES_RUNTIME"
+  REPROSTIM_CAPTURE_CLEAN="apt-get remove --purge -y $REPROSTIM_CAPTURE_PACKAGES_DEV && apt-get autoremove -y"
+  # Extend with packages hold if runtime packages env exist
+  if [[ -n "$REPROSTIM_CAPTURE_PACKAGES_RUNTIME" ]]; then
+      REPROSTIM_CAPTURE_CLEAN="apt-mark hold $REPROSTIM_CAPTURE_PACKAGES_RUNTIME && $REPROSTIM_CAPTURE_CLEAN"
+  fi
 fi
 
 
