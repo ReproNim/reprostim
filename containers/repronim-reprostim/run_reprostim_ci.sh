ecffe8607cf1bf6a7a0c8ee859d1aa4fca92ae34
Vadim Melnik <vmelnik@docsultant.com>
2025-10-23 17:58:34 +0300

    Fix .X11 socket/Xauthority issues to start psychopy UI from docker container, #188, #193.

--- containers/repronim-reprostim/run_reprostim_ci.sh
+++ containers/repronim-reprostim/run_reprostim_ci.sh
@@ -26,10 +26,11 @@ elif [[ "$REPROSTIM_CONTAINER_TYPE" == "singularity" ]]; then
   REPROSTIM_CONTAINER_IMAGE="${REPROSTIM_CONTAINER_IMAGE:-./repronim-reprostim-${REPROSTIM_VERSION}.sing}"
 fi
 
-# Calculate entry point
+# Calculate entry point and command to run inside container
 REPROSTIM_CONTAINER_ENTRYPOINT=""
-if [ "$REPROSTIM_CONTAINER_RUN_MODE" = "reprostim-videocapture" ]; then
-    REPROSTIM_CONTAINER_APP="reprostim-videocapture"
+if [ "$REPROSTIM_CONTAINER_RUN_MODE" = "reprostim-videocapture" ] || [ "$REPROSTIM_CONTAINER_RUN_MODE" = "psychopy" ]; then
+    REPROSTIM_CONTAINER_APP="$REPROSTIM_CONTAINER_RUN_MODE"
+    # clear/remove python entrypoint for standalone apps
     if [ "$REPROSTIM_CONTAINER_TYPE" = "docker" ]; then
         REPROSTIM_CONTAINER_ENTRYPOINT="--entrypoint="
     fi
@@ -54,6 +55,19 @@ log "  [IMAGE]          : ${REPROSTIM_CONTAINER_IMAGE}"
 log "  [OVERLAY]        : ${REPROSTIM_OVERLAY}"
 log "  [ARGS]           : $*"
 
+#
+# Prepare XAUTHORITY path (fallback to $HOME/.Xauthority)
+# also before running docker container with psychopy UI allow
+# access to X server with:
+#
+#  xhost +local:root
+#    or
+#  xhost +local:docker
+#
+# and optionally to quiet Podman (when "Emulate Docker CLI" listed in logs):
+#  sudo touch /etc/containers/nodocker
+#
+XAUTHORITY_HOST="${XAUTHORITY:-$HOME/.Xauthority}"
 
 if [[ "$REPROSTIM_CONTAINER_TYPE" == "docker" ]]; then
   docker run --rm -i ${DOCKER_TTY} \
@@ -61,6 +75,8 @@ if [[ "$REPROSTIM_CONTAINER_TYPE" == "docker" ]]; then
     -v "${REPROSTIM_PATH}:${REPROSTIM_PATH}" \
     -w "${REPROSTIM_PATH}" \
     --env DISPLAY=$DISPLAY \
+    -v "${XAUTHORITY_HOST}:${XAUTHORITY_HOST}:ro" \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
     ${REPROSTIM_OVERLAY} ${REPROSTIM_CONTAINER_IMAGE} \
     ${REPROSTIM_CONTAINER_APP} "$@"
 elif [[ "$REPROSTIM_CONTAINER_TYPE" == "singularity" ]]; then
