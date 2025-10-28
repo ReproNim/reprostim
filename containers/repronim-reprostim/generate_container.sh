8284857454ab7779ee855caae82897c6629acdb5
Vadim Melnik <vmelnik@docsultant.com>
2025-10-28 17:34:49 +0200

    Add zbar dependency to container to fix CI-CD errors, #189, #193.

--- containers/repronim-reprostim/generate_container.sh
+++ containers/repronim-reprostim/generate_container.sh
@@ -61,7 +61,7 @@ generate() {
     --install build-essential pkg-config git \
           sudo \
           libgtk-3-dev libwebkit2gtk-4.1 libwxgtk3.2-dev libwxgtk-media3.2-dev libwxgtk-webview3.2-dev libcanberra-gtk3-module \
-          libsdl2-2.0-0 libusb-1.0-0-dev portaudio19-dev libasound2-dev pulseaudio pavucontrol pulseaudio-utils \
+          libsdl2-2.0-0 libzbar0 libusb-1.0-0-dev portaudio19-dev libasound2-dev pulseaudio pavucontrol pulseaudio-utils \
           vim wget strace time ncdu gnupg curl procps pigz less tree python3 python3-pip \
           "${REPROSTIM_CAPTURE_PACKAGES_RUNTIME}" \
           "${REPROSTIM_CAPTURE_PACKAGES_DEV}" \
